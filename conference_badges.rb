747076b054eaa00fc9af7c7ead88d3aa16db4eeb
Sarah Drake <jussaradrake@outlook.com>
2019-08-22 13:41:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-myarray = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Mat".]
+myarray = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Mat"]
 def batch_badge_creator(array)
   myarray = []
   myarray.each do |name|
