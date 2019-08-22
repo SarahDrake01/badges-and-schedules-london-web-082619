36eaf15c7da730fb7623cd1e10684859e901c09a
Sarah Drake <jussaradrake@outlook.com>
2019-08-22 13:36:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,11 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(badges)
-  
+def batch_badge_creator(array)
+  array = []
+  array.each do |name|
+    array.push("Hello, my name is #{name}.")
+  end
+  return array
   
 end
