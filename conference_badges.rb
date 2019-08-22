b591d05754c28c568edcaa60d71b1b9cdb7a87df
Sarah Drake <jussaradrake@outlook.com>
2019-08-22 14:08:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,14 @@
+name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Mat"]
+
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-myarray = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Mat"]
 def batch_badge_creator(myarray)
-  myarray = []
+  newarray = []
   myarray.each do |name|
-    myarray.push("Hello, my name is #{name}.")
+    newarray.push("Hello, my name is #{name}.")
+    
   end
-  return myarray
+  return newarray
 end
